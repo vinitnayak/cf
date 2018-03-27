c67d990e11dbb1e0ece88e48f3e2e0f1645886b8
Sudhir PallaPolu <SudhirP@bsihq.com>
2018-03-27 15:35:31 -0400

    sample code, remove later.

--- uitests/server/express.js
+++ uitests/server/express.js
@@ -10,4 +10,23 @@ if (process.env.NODE_ENV === "development" || process.env.NODE_ENV ==='productio
     console.log('Starting Web server at http://localhost:'+port+'/');
     console.log('======================================================>');
     console.log('Starting JSON Server for mock REST APIs');
+   /* app.get('/svc/v1/*', function (req, res) {        
+        var options = {
+            host: url,
+            port:300024,
+            path: '/*',
+            method: 'GET'
+          };
+          var response = {};
+        http.request(options, function(res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+              console.log('BODY: ' + chunk);
+              response = chunk;
+            });
+          }).end();
+          res.send(response);
+      });*/
 }
